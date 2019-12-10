e49baf0fd6f76a61313ed6bc49cd695c5f91c959
TosinKristine <yadekatosin@yahoo.com>
2019-12-10 14:31:57 +0100

    Readme Update

--- dist/ngx-seerbit/seerbit.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { SeerBitComponent as ɵc } from './lib/seer-bit.component';
-export { SeerBitButtonDirective as ɵa } from './lib/seer-bit.directive';
-export { SeerbitService as ɵb } from './lib/seerbit-service';
